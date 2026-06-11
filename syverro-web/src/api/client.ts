37fdd4492e4d1543beafc60d94a728e8510930c8
Nika Alma <maramorghan@gmail.com>
2026-06-11 18:59:46 +0300

    fix: update API_BASE_URL to production Railway URL

--- syverro-web/src/api/client.ts
+++ syverro-web/src/api/client.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8000'
-
+const API_BASE_URL = 'https://syverro-production.up.railway.app'
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
